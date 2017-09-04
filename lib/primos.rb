141e6e86cc418aadbf18459c4c9c035ec4777417
Elizeu Costa <sinhorinho@gmail.com>
2017-09-04 16:36:41 -0300

    implementation of the algorithm

--- lib/primos.rb
+++ lib/primos.rb
@@ -1,2 +1,35 @@
 class Primos
+
+	def self.listar(n)
+
+		array = Array.new
+
+		elem = 2
+
+		while array.size < n do 
+
+			if is_numero_primo?(elem)
+				array.push(elem)
+			end
+			elem = elem + 1
+		end
+
+		return array
+
+	end
+
+	def self.is_numero_primo?(n)
+
+		primo = true
+
+		for i in 2..(Math.sqrt(n)) do
+			if n % i == 0 then
+				primo = false
+			end
+		end
+
+		return primo
+
+	end
+
 end
